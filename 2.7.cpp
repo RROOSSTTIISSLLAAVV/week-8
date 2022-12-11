c5df8b288d68edbebf84146f6ed03b503bb48b1d
RROOSSTTIISSLLAAVV <53853794+RROOSSTTIISSLLAAVV@users.noreply.github.com>
2022-12-11 22:44:44 +0200

    Update 2.7.cpp

--- 2.7.cpp
+++ 2.7.cpp
@@ -9,9 +9,7 @@ int main(void) {
 	cout << "Number of iterations? ";
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		pi4 += pow(-1.0, i) / (1 + 2 * i);
-	}
+	for (int i = 0; i < n; i++) pi4 += pow(-1.0, i) / (1 + 2 * i);
 
 	cout.precision(20);
 	cout << "Pi = " << (pi4 * 4.) << endl;
