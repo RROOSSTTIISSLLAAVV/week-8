2f406ff5cf72ecf2651dda9b1f62e6cb2f7231b8
RROOSSTTIISSLLAAVV <53853794+RROOSSTTIISSLLAAVV@users.noreply.github.com>
2022-12-11 22:55:21 +0200

    Update 2.10.cpp

--- 2.10.cpp
+++ 2.10.cpp
@@ -7,17 +7,21 @@ int main(void) {
 	cout << "n: ";
 	cin >> n;
 
-	cout << '+';
-	for (int i = 0; i < n*2; i++) cout << '-';
-	cout << '+' << endl;
-	for (int i = 0; i < n; i++) {
-		cout << '|';
-		for (int j = 0; j < n*2; j++) cout << ' ';
-		cout << '|' << endl;
+	if (n < 1 || n > 50) cout << "0 < n < 50";
+
+	else {
+		cout << '+';
+		for (int i = 0; i < n * 2; i++) cout << '-';
+		cout << '+' << endl;
+		for (int i = 0; i < n; i++) {
+			cout << '|';
+			for (int j = 0; j < n * 2; j++) cout << ' ';
+			cout << '|' << endl;
+		}
+		cout << '+';
+		for (int i = 0; i < n * 2; i++) cout << '-';
+		cout << '+';
 	}
-	cout << '+';
-	for (int i = 0; i < n * 2; i++) cout << '-';
-	cout << '+';
 
 	return 0;
 }
